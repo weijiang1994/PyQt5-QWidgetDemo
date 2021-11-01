31af10a0a03732f4413373603caf90c24160566e
jiangwei1 <jiangwei1@kylinos.cn>
2021-11-01 10:46:48 +0800

    add movewidget

--- 02_movewidget/movewidget.py
+++ 02_movewidget/movewidget.py
@@ -8,7 +8,8 @@ file: movewidget.py
 """
 from PyQt5.QtCore import QObject, QPoint, Qt
 from PyQt5.QtWidgets import QWidget
-from PyQt5.Qt import QEvent, QMouseEvent
+from PyQt5.Qt import QEvent
+from PyQt5.QtGui import QMouseEvent
 import traceback
 
 
@@ -24,16 +25,15 @@ class MoveWidget(QObject):
     def eventFilter(self, watched: QObject, event: QEvent) -> bool:
         try:
             if self.widget != 0 and watched == self.widget:
-                # mouse_event = QMouseEvent(event)
-                print(event)
                 mouse_event = event
                 if mouse_event.type() == QEvent.MouseButtonPress:
-                    if self.left_button and mouse_event.button() == Qt.LeftButton:
+                    if self.left_button and mouse_event.button() != Qt.LeftButton:
                         return False
 
                     if self.widget.rect().contains(mouse_event.pos()):
                         self.last_point = mouse_event.pos()
                         self.pressed = True
+
                 elif mouse_event.type() == QEvent.MouseMove and self.pressed:
                     offset_x = mouse_event.pos().x() - self.last_point.x()
                     offset_y = mouse_event.pos().y() - self.last_point.y()
@@ -42,7 +42,7 @@ class MoveWidget(QObject):
 
                     if self.in_control:
                         offset = 20
-                        xy_out = (x + self.widget.width() < offset or y + self.widget.height())
+                        xy_out = (x + self.widget.width() < offset or y + self.widget.height() < offset)
                         wh_out = False
                         w = QWidget(self.widget.parent())
                         if w != 0:
@@ -51,11 +51,12 @@ class MoveWidget(QObject):
                         if xy_out or wh_out:
                             return False
                     self.widget.move(x, y)
+
                 elif mouse_event.type() == QEvent.MouseButtonRelease and self.pressed:
                     self.pressed = False
+            return super(MoveWidget, self).eventFilter(watched, event)
         except:
             traceback.print_exc()
-        return super(MoveWidget, self).eventFilter(watched, event)
 
     def set_left_button(self, left_button):
         self.left_button = left_button
--- 02_movewidget/run.py
+++ 02_movewidget/run.py
@@ -6,26 +6,23 @@ file: run.py
 @time: 2021/10/31 22:05
 @desc:
 """
+from mw_frame import Ui_Form
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QHBoxLayout
 import sys
 from movewidget import MoveWidget
 import traceback
 
 
-class MoveWidgetWin(QWidget):
+class MoveWidgetWin(QWidget, Ui_Form):
     def __init__(self):
-        try:
-            super(MoveWidgetWin, self).__init__()
-            self.layout = QHBoxLayout()
-            self.btn1 = QPushButton(self)
-            self.btn1.setGeometry(10, 10, 250, 25)
-            self.btn1.setText('按住我拖动(仅限左键拖动)')
-            self.mv1 = MoveWidget(self)
-            self.mv1.set_widget(self.btn1)
-            self.layout.addWidget(self.btn1)
-            self.setLayout(self.layout)
-        except:
-            traceback.print_exc()
+        super(MoveWidgetWin, self).__init__()
+        self.setupUi(self)
+        self.btn1.setGeometry(10, 10, 250, 25)
+        # self.btn2.setGeometry(10, 40, 250, 25)
+        mw1 = MoveWidget(self)
+        mv2 = MoveWidget(self)
+        mw1.set_widget(self.btn1)
+        # mv2.set_widget(self.btn2)
 
 
 if __name__ == '__main__':
